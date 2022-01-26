cf68ebd5a39f989ddd288a06276e9c8db3ee32a6
tewei <teweishieh@gmail.com>
2022-01-26 16:50:06 +0800

    Update

--- app.py
+++ app.py
@@ -166,15 +166,11 @@ def handle_message(event):
             template=ButtonsTemplate(
                 title='這是ButtonsTemplate',
                 text='ButtonsTemplate可以傳送text,uri',
-                thumbnail_image_url='顯示在開頭的大圖片網址',
+                thumbnail_image_url='https://ntumed.github.io/images/logo01.png',
                 actions=[
                     MessageTemplateAction(
                         label='ButtonsTemplate',
                         text='ButtonsTemplate'
-                    ),
-                    URITemplateAction(
-                        label='VIDEO1',
-                        uri='影片網址'
                     ),
                     PostbackTemplateAction(
                         label='postback',
